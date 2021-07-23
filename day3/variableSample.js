cef118bc2fe25e7d6d27b941d554c9ba5ca7c9f0
Muthu <84056393+mkandanmk7@users.noreply.github.com>
2021-07-23 16:46:35 +0530

    Create variableSample.js

---
+++ day3/variableSample.js
@@ -0,0 +1,49 @@
+//1.Declare four variables without assigning values
+//  and print them in console?
+
+let A, B, C, D;
+console.log(A); // undefined
+console.log(B); // undefined
+console.log(C); // undefined
+console.log(C); // undefined
+
+//2.How to get value of the variable myvar as output.
+
+var myvar = 1;
+console.log(myvar); //1
+
+//3.Declare variables to store your first name, last name,
+// marital status, country and age in multiple lines.
+
+let firstName = "Muthu";
+let LastName = "manikandan";
+let maritalStatus = "Unmarried";
+let country = "India";
+let age = 25;
+
+//4.Declare variables to store your first name, last name, marital status, 
+//country and age in a single line.
+
+let firstName = "Muthu", LastName = "manikandan", maritalStatus = "Unmarried", country = "India", age = 25;
+
+
+//5.Declare variables and assign string,.
+// boolean, undefined and null data types.
+
+let name = "muthu"; // string data type,
+let isAlive = true; // boolean data type,
+let is = undefined; // undefined,
+let isNull = null// nulldata types.
+
+
+//6. Convert the string to integer
+
+
+let Name = "9";
+let convertString = parseInt(Name); // number type,
+
+let convert = +(Name); // number type,
+
+let c = Number(Name); //number type.
+
+
